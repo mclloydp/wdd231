69d188f022907410a755261245aacda9eefe4c18
McLloyd Pise <mpise@byupathway.edu>
2025-01-18 06:08:31 +0200

    update image formats and adjust styles; modify date options in script

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -7,5 +7,5 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // toLocaleDateString 
-const options = {weekday: 'long',day: 'numeric', month: 'long', year: 'numeric', time: 'numeric'};
+const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
